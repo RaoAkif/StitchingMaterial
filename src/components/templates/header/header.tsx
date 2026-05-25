4232a97278db07931330088c7b9d2a0ba23691fc
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2026-05-25 19:46:34 +0500

    Update header.tsx

--- src/components/templates/header/header.tsx
+++ src/components/templates/header/header.tsx
@@ -28,7 +28,7 @@ export const Header = (props: BoxProps) => {
           alt={t('common.logoImageAltText')}
           title={t('common.logoImageAltText')}
           display={{ base: 'none', md: 'block', lg: 'block' }}
-          height={{ base: '45px', md: '70px', lg: '70px' }}
+          height={{ base: '40px', md: '60px', lg: '60px' }}
           mt={{ base: 0, md: 2, lg: 3 }}
         />
         <Box
@@ -37,7 +37,7 @@ export const Header = (props: BoxProps) => {
           alt={t('common.logoImageAltText')}
           title={t('common.logoImageAltText')}
           display={{ base: 'block', md: 'none', lg: 'none' }}
-          height={{ base: '52px', md: '60px', lg: '60px' }}
+          height={{ base: '56px', md: '64px', lg: '64px' }}
         />
       </Link>
       <LanguageSelector />
